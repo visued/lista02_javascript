097e35d8ca4e954fb8838b88d1062ad89edb1aad
Victor Vinicius Sued Flavio <victor-sued@uam.com.br>
2018-05-17 14:11:16 -0300

    Alterações no javascript

--- js/func.js
+++ js/func.js
@@ -1,15 +1,29 @@
-function change(){
-    var n1 = document.getElementById("n1").value;
-    var n2 = document.getElementById("n2").value;
-    
-    if ( ((Number(n1)+Number(n2))/2) >= 7){
-        document.getElementById("n3").innerHTML = "Aprovado";
-        console.log(((Number(n1)+Number(n2))/2));
-    }else if ((((Number(n1)+Number(n2))/2) >= 3) && ( ((Number(n1)+Number(n2))/2) <= 7)){
-        document.getElementById("n3").innerHTML = "Exame";
-        console.log(((Number(n1)+Number(n2))/2));
-    }else{
-        document.getElementById("n3").innerHTML = "Reprovado";
+function ex01() {
+    var n1 = Number(document.getElementById("ex01_n1").value);
+    var n2 = Number(document.getElementById("ex01_n2").value);
+    var n3 = Number(document.getElementById("ex01_n3").value);
+    var n4 = Number(document.getElementById("ex01_n4").value);
+
+    if ((n1 + n2 + n3 + n4) / 4 >= 7) {
+        document.getElementById("ex01_result").innerHTML = "Aprovado";
+        document.getElementById("ex01_result").style.color = "green";
+    } else {
+        document.getElementById("ex01_result").innerHTML = "Reprovado";
+        document.getElementById("ex01_result").style.color = "red";
     }
 
 }
+
+function ex02() {
+    var n1 = Number(document.getElementById("ex01_n1").value);
+    var n2 = Number(document.getElementById("ex02_n2").value);
+
+    if ((n1 + n2) / 2 >= 7) {
+        document.getElementById("n3").innerHTML = "Aprovado";
+    } else if (((n1 + n2) / 2 >= 3) && ((n1 + n2) / 2 <= 7)) {
+            document.getElementById("ex02_result").innerHTML = "Exame";
+        } else {
+            document.getElementById("ex02_result").innerHTML = "Reprovado";
+        }
+
+    }
